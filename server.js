4ff6f1636f4211457aeca9133ab2c01dd9b7fd66
far312n <130723258+far312n@users.noreply.github.com>
2023-10-20 11:03:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const requestIp = require('request-ip');
 const fs = require('fs');
 const app = express();
 const port = 3000;
-const AWS = require("aws-sdk");
-const s3 = new AWS.S3()
 
 app.use(express.json());
 app.use(express.static('public'));
@@ -16,21 +14,15 @@ app.post('/server', (req, res) => {
   const data = `Latitude: ${latitude}, Longitude: ${longitude}, Maps: http://maps.google.com/maps?z=12&t=m&q=loc:${latitude}+${longitude}\n`;
 
   // Append data to a text file
-  // fs.appendFile('my-location.txt', data, (err) => {
-  //   if (err) {
-  //     console.error(err);
-  //     res.status(500).send('Error saving location data to file');
-  //   } else {
-  //     console.log('Location data saved to file.');
-  //     res.status(200).send('Location data saved to file.');
-  //   }
-  // });
-
-  s3.putObject({
-      Body: JSON.stringify({key:"value"}),
-      Bucket: "cyclic-inquisitive-elk-bonnet-ap-northeast-1",
-      Key: "my-location.txt",
-  }).promise()
+  fs.appendFile('my-location.txt', data, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error saving location data to file');
+    } else {
+      console.log('Location data saved to file.');
+      res.status(200).send('Location data saved to file.');
+    }
+  });
 });
 
 port == process.env.port || port
